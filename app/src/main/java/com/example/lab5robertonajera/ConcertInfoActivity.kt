a88939a2a9c85d46708ac2b2cdfccfec0bdac6c6
Ultimate-Truth-Seeker <96703353+Ultimate-Truth-Seeker@users.noreply.github.com>
2024-09-03 23:07:23 -0600

    Añadido Navigation Drawer y Ajuste de webm

--- app/src/main/java/com/example/lab5robertonajera/ConcertInfoActivity.kt
+++ app/src/main/java/com/example/lab5robertonajera/ConcertInfoActivity.kt
@@ -9,15 +9,21 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Card
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -25,33 +31,52 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.lab5robertonajera.ui.theme.TodoEventoTheme
+import kotlinx.coroutines.launch
 
 class ConcertInfoActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TodoEventoTheme {
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            title = { Text("TodoEventos") },
-                            actions = {
-                                IconButton(onClick = { }) {
-                                    Icon(
-                                        painter = painterResource(id = R.drawable.place_image1),
-                                        contentDescription = "Menu"
-                                    )
-                                }
-                            },
-                            modifier = Modifier.background(color = Color(0xFF6200EE)),
-                        )
-                    }, content = {
-                        ConcertInfoScreen()
-                    }
-                )
+                ConcertInfoLayout()
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ConcertInfoLayout(){
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            DrawerContent()
+        }
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("TodoEventos") },
+                    actions = {
+                        IconButton(onClick = {
+                            scope.launch {
+                                drawerState.open()
+                            }
+                        }) {
+                            Icon(
+                                Icons.Default.MoreVert,
+                                contentDescription = "Menu"
+                            )
+                        }
+                    },
+                    modifier = Modifier.background(color = Color(0xFF6200EE)),
+                )
+            }, content = {
+                ConcertInfoScreen()
+            }
+        )
     }
 }
 
@@ -150,4 +175,22 @@ fun getSampleConcerts(): List<Concert> {
         Concert("Title 3", "Supporting text", R.drawable.concert_image3),
         Concert("Title 4", "Supporting text", R.drawable.concert_image3)
     )
+}
+
+@Composable
+fun DrawerContent() {
+    Column(modifier = Modifier
+        .fillMaxHeight()
+        .padding(16.dp)
+        .background(color = Color.White)
+        .width(128.dp)) {
+        Text(
+            text = "Navigation Drawer",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            )
+        Text(text = "Opción 1", modifier = Modifier.padding(8.dp))
+        Text(text = "Opción 2", modifier = Modifier.padding(8.dp))
+        Text(text = "Opción 3", modifier = Modifier.padding(8.dp))
+    }
 }
